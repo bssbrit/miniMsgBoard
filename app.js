069cc937930a7a37ae3f81d92da99bf876fa1871
bssbrit <brunosabrito2@gmail.com>
2023-11-21 00:46:37 -0300

    basic routing and view engines templates ready, next task is to loop through the sample message array and connect to a DB

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-
+var msgRouter = require("./routes/newMsg");
 var app = express();
 app.listen(3000);
 // view engine setup
@@ -21,7 +21,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
+app.use("/new", msgRouter);
+/* app.get("/new", (req, res) => {
+  res.render("newMsg", { title: "New" });
+}); */
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
