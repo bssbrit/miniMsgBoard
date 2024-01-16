da6a3aa1f4a2a76443dfadf7562bd3597d7a1ad0
bssbrit <brunosabrito2@gmail.com>
2024-01-15 23:45:07 -0300

    fetched the data.json

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data.json");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <button onClick={() => console.log(data)}> Data Log</button>
+    </>
+  );
+}
+export default App;
